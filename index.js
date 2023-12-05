12c22ef72c4a2b7a0110b92a4fa7b09882890377
Cleiton <cleitonmaciel123@gmail.com>
2023-12-05 19:04:24 -0300

    registro e banco de dados

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql'); // Importa a biblioteca mysql
+
+// Body Parser analisa o dados enviados pelo formulário HTML
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const {Sequelize, DataTypes} = require('sequelize');
+const sequelize = new Sequelize('teste', 'Shadow', 'Shadow2468', {
+    host: "localhost",
+    dialect: "mysql"
+})
+
+sequelize.authenticate().then(function(){
+    console.log("Conectado com sucesso!")
+}).catch(function(){
+    console.log("Falha ao se conectar:" + erro)
+})
+
+const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING
+    },
+    senha: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    }
+  });
+  
+  
+  app.post("/registro", async function(req, res) {
+    const { Username, password, email } = req.body;
+  
+    try {
+      const newUser = await User.create({ username: Username, senha: password, email: email });
+      res.status(200).send('Usuário registrado com sucesso');
+    } catch (error) {
+      console.error('Erro ao inserir usuário:', error);
+      res.status(500).send('Erro ao registrar usuário');
+    }
+  });
 
 // Serve arquivos estáticos de 'css' no diretório '/css'
 app.use('/css', express.static(__dirname + '/css'));
 
 app.use('/imagens', express.static(__dirname + '/imagens'));
 
-// Constantes usadas para a autenticação
-const usuario = "Shadow";
-const senha = "sdw";
-
-// Body Parser analisa o dados enviados pelo formulário HTML
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Função da raiz
 app.get("/", function(req, res) {
@@ -30,14 +65,23 @@ app.get("/usuario", function(req, res) {
 });
 
 // Rota para lidar com o envio do formulário de login
-app.post("/login", function(req, res) {
+app.post("/login", async function(req, res) {
     const { username, password } = req.body;
-    if (username === usuario && password === senha) {
-        res.redirect("/usuario");
-    } else {
-        res.redirect("/login");
+  
+    try {
+      const user = await User.findOne({ where: { username: username, senha: password } });
+  
+      if (user) {
+        res.redirect('/usuario')
+      } else {
+        res.redirect('/login')
+      }
+    } catch (error) {
+      console.error('Erro ao verificar login:', error);
+      res.status(500).send('Erro ao verificar login');
     }
-});
+  });
+
 
 // Rotas para categorias e outros recursos
 app.get("/vendas", function(req, res) {
